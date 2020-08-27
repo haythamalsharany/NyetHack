5cd0c708e8bbff6e7ae28331e5b49f469dc05fc3
haytham taha alsharany <talsharany@gmail.com>
2020-08-27 16:02:11 +0300

    control flow

--- src/Game.kt
+++ src/Game.kt
@@ -1,26 +1,25 @@
 fun main() {
-  var  healthPoint :Int =100
-    val playerName:String ="Madrigal"
-    var isBlessed :Boolean=true
+    var healthPoint: Int = 100
+    val playerName: String = "Madrigal"
+    var isBlessed: Boolean = true
 
-    if(healthPoint == 100){
-        println(playerName+" is an excelant  condition!")
-    }else if(healthPoint in 90..99)
-        println(playerName+" has a few scraches  ")
-    else if(healthPoint in 75..89) {
-        if(isBlessed)
+    if (healthPoint == 100) {
+        println(playerName + " is an excelant  condition!")
+    } else if (healthPoint in 90..99)
+        println(playerName + " has a few scraches  ")
+    else if (healthPoint in 75..89) {
+        if (isBlessed)
             println(playerName + " has some minor wounds but is healing quite ! ")
         else
-        println(playerName + " has some minor wounds  ")
-    }
-    else if(healthPoint in 15..74)
-        println(playerName+" looks prety hurt  ")
-    else if(healthPoint in 0..14)
-        println(playerName+" is in awful condition! ")
+            println(playerName + " has some minor wounds  ")
+    } else if (healthPoint in 15..74)
+        println(playerName + " looks prety hurt  ")
+    else if (healthPoint in 0..14)
+        println(playerName + " is in awful condition! ")
     else
         println("some thing wrong  ")
 
 
-    println(playerName +"&"+healthPoint)
+    println(playerName + "&" + healthPoint)
 
 }
